73261e56ea179e4738ad994287a181f0c5421edc
Alanna <116979966+alannagab@users.noreply.github.com>
2023-08-28 11:19:31 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import openai
+
+openai.api_key = 'sk-'
+
+temperature = 0
+max_tokens = 256
+messages = [
+    {"role": "system", "content": "You are a kind helpful assistant."},
+]
+
+while True:
+    message = input("User : ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature = temperature,
+            max_tokens = max_tokens
+        )
+    
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
